fd1bac971f5a4cfa99465500deed5a8a7a9fe6e8
Felix <felixsavero78@gmail.com>
2022-10-24 12:57:12 +0700

    Save notes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
-console.log('Going to make RESTful APII');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
-const a = 1;
-if (a === 1) {
-  console.log(a);
-}
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  server.route(routes);
+
+  try {
+    await server.start();
+    console.log(`Server running at ${server.info.uri}`);
+  } catch (errMsg) {
+    console.error(errMsg);
+  }
+};
+
+init();
+
+// Front End : http://notesapp-v1.dicodingacademy.com/
